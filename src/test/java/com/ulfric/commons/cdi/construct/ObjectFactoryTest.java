ad81238979b2f8cb87ac5bd11deb1d6b7fac9d23
Adam Harrison <ad@mharrison.us>
2017-01-13 17:29:53 -0500

    Failing test

---
+++ src/test/java/com/ulfric/commons/cdi/construct/ObjectFactoryTest.java
@@ -0,0 +1,18 @@
+package com.ulfric.commons.cdi.construct;
+
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+import com.ulfric.verify.Verify;
+
+@RunWith(JUnitPlatform.class)
+public class ObjectFactoryTest {
+
+	@Test
+	void testNew()
+	{
+		Verify.that(() -> new ObjectFactory()).runsWithoutExceptions();
+	}
+
+}
