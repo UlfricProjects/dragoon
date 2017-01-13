33921f69237691e192822ab8b16867f9c5ddeb72
Adam Harrison <ad@mharrison.us>
2017-01-13 18:47:49 -0500

    Failing test

---
+++ src/test/java/com/ulfric/commons/cdi/BindingTest.java
@@ -0,0 +1,18 @@
+package com.ulfric.commons.cdi;
+
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+import com.ulfric.verify.Verify;
+
+@RunWith(JUnitPlatform.class)
+public class BindingTest {
+
+	@Test
+	void testNew()
+	{
+		Verify.that(() -> new Binding<>(null)).runsWithoutExceptions();;
+	}
+
+}
