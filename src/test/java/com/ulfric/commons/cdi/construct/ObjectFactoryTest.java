7666051a159eef81a4142e37473f1157346f687f
Adam Harrison <ad@mharrison.us>
2017-01-13 17:44:57 -0500

    Failing test

--- src/test/java/com/ulfric/commons/cdi/construct/ObjectFactoryTest.java
+++ src/test/java/com/ulfric/commons/cdi/construct/ObjectFactoryTest.java
@@ -1,5 +1,6 @@
 package com.ulfric.commons.cdi.construct;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
@@ -9,10 +10,24 @@ import com.ulfric.verify.Verify;
 @RunWith(JUnitPlatform.class)
 public class ObjectFactoryTest {
 
+	private ObjectFactory factory;
+
+	@BeforeEach
+	void init()
+	{
+		this.factory = ObjectFactory.newInstance();
+	}
+
 	@Test
 	void testNewInstance()
 	{
 		Verify.that(ObjectFactory::newInstance).suppliesUniqueValues();
+	}
+
+	@Test
+	void testCreateSubfactory()
+	{
+		Verify.that(this.factory::subfactory).suppliesUniqueValues();
 	}
 
 }
