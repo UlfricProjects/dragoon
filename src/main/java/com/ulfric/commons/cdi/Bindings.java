0920b02795cc871836dfd0541e37fe3adc809d7e
Adam Harrison <ad@mharrison.us>
2017-01-15 03:22:18 -0500

    Thread safety for Bindings

--- src/main/java/com/ulfric/commons/cdi/Bindings.java
+++ src/main/java/com/ulfric/commons/cdi/Bindings.java
@@ -2,10 +2,13 @@ package com.ulfric.commons.cdi;
 
 import java.util.IdentityHashMap;
 import java.util.Map;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 final class Bindings extends Child<Bindings> {
 
 	private final Map<Class<?>, Class<?>> bindings = new IdentityHashMap<>();
+	private final ReadWriteLock mutex = new ReentrantReadWriteLock();
 
 	Bindings()
 	{
@@ -24,19 +27,26 @@ final class Bindings extends Child<Bindings> {
 
 	Class<?> getRegisteredBinding(Class<?> request)
 	{
+		this.mutex.readLock().lock();
+
 		Class<?> binding = this.bindings.get(request);
 
 		if (binding == null && this.hasParent())
 		{
-			return this.getParent().getRegisteredBinding(request);
+			binding = this.getParent().getRegisteredBinding(request);
 		}
 
+		this.mutex.readLock().unlock();
 		return binding;
 	}
 
 	void registerBinding(Class<?> request, Class<?> implementation)
 	{
+		this.mutex.writeLock().lock();
+
 		this.bindings.put(request, implementation);
+
+		this.mutex.writeLock().unlock();
 	}
 
 }
