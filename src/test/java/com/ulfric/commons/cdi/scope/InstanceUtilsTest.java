e0ee7e8ea3eb5f707f02a9950d1cb40d1db5c7a6
Adam Harrison <ad@mharrison.us>
2017-01-16 00:29:01 -0500

    Failing test

--- src/test/java/com/ulfric/commons/cdi/scope/InstanceUtilsTest.java
+++ src/test/java/com/ulfric/commons/cdi/scope/InstanceUtilsTest.java
@@ -1,15 +1,21 @@
 package com.ulfric.commons.cdi.scope;
 
+import org.junit.jupiter.api.Test;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
 
 import com.ulfric.testing.Util;
 import com.ulfric.testing.UtilTestBase;
+import com.ulfric.verify.Verify;
 
 @RunWith(JUnitPlatform.class)
 @Util(InstanceUtils.class)
 public class InstanceUtilsTest extends UtilTestBase {
 
-	
+	@Test
+	void testCreateInstanceOrNull_object_nonnull()
+	{
+		Verify.that(InstanceUtils.createInstanceOrNull(Object.class)).isNotNull();
+	}
 
 }
