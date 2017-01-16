9e7c0a0481417918997d183eb7f5caf1ab9701b1
Adam Harrison <ad@mharrison.us>
2017-01-16 00:26:41 -0500

    Failing test

---
+++ src/test/java/com/ulfric/commons/cdi/scope/InstanceUtilsTest.java
@@ -0,0 +1,14 @@
+package com.ulfric.commons.cdi.scope;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+import com.ulfric.testing.UtilTestBase;
+
+@RunWith(JUnitPlatform.class)
+@Util(InstanceUtils.class)
+public class InstanceUtilsTest extends UtilTestBase {
+
+	
+
+}
