c5a072ef0bd131e49e29bdcd9d5e6ccdd2de7ecd
Adam Harrison <ad@mharrison.us>
2017-01-16 00:27:51 -0500

    Fix compiling

--- src/test/java/com/ulfric/commons/cdi/scope/InstanceUtilsTest.java
+++ src/test/java/com/ulfric/commons/cdi/scope/InstanceUtilsTest.java
@@ -3,6 +3,7 @@ package com.ulfric.commons.cdi.scope;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
 
+import com.ulfric.testing.Util;
 import com.ulfric.testing.UtilTestBase;
 
 @RunWith(JUnitPlatform.class)
