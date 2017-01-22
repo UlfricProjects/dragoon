34ec040c24bb925cdc55617eb12f27da876679b5
Adam Harrison <ad@mharrison.us>
2017-01-22 18:32:35 -0500

    Make scopes @Inherited

--- src/main/java/com/ulfric/commons/cdi/scope/Shared.java
+++ src/main/java/com/ulfric/commons/cdi/scope/Shared.java
@@ -1,6 +1,7 @@
 package com.ulfric.commons.cdi.scope;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -8,6 +9,7 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @Scope
+@Inherited
 public @interface Shared {
 
 }
