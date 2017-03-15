17b585b3216d01f3486576cf606351b5bbefcdc8
insou22 <zac.kologlu@gmail.com>
2017-03-15 21:59:12 +1100

    Make the tests beautiful

--- src/test/java/com/ulfric/dragoon/bean/BeansTest.java
+++ src/test/java/com/ulfric/dragoon/bean/BeansTest.java
@@ -4,12 +4,14 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.lang.reflect.Method;
 
 import org.junit.jupiter.api.Test;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
 
 import com.ulfric.commons.bean.Bean;
+import com.ulfric.commons.exception.Try;
 import com.ulfric.testing.Util;
 import com.ulfric.testing.UtilTestBase;
 import com.ulfric.verify.Verify;
@@ -32,30 +34,51 @@ public class BeansTest extends UtilTestBase {
 	}
 
 	@Test
-	void testCreate_interfaceBean()
+	void testCreate_interfaceBean_suppliesUnique()
 	{
 		Verify.that(() -> Beans.create(InterfaceBean.class)).suppliesUniqueValues();
+	}
+
+	@Test
+	void testBean_setGetValues_wrapped()
+	{
 		InterfaceBean bean = Beans.create(InterfaceBean.class);
 
 		Verify.that(() -> bean.setString("foo")).runsWithoutExceptions();
 		Verify.that(bean::getString).valueIsEqualTo("foo");
-
-		Verify.that(() -> bean.setTrue(true)).runsWithoutExceptions();
-		Verify.that(bean::isTrue).valueIsEqualTo(true);
-
-		Verify.that(bean::notAGetter).doesThrow(AbstractMethodError.class);
-		Verify.that(bean::getKiddingItsVoid).doesThrow(AbstractMethodError.class);
-		Verify.that(() -> bean.getWaitIsThisASetter(null)).doesThrow(AbstractMethodError.class);
-
-		Verify.that(Beans.create(InterfaceBean.class).getClass()).isEqualTo(bean.getClass());
 	}
 
 	@Test
-	void testCreate_interface_carriesAnnotationsFromParent()
+	void testBean_setGetValues_primitive()
 	{
 		InterfaceBean bean = Beans.create(InterfaceBean.class);
 
-		Verify.that(bean.getClass().isAnnotationPresent(FooAnnotation.class)).isTrue();
+		Verify.that(() -> bean.setTrue(true)).runsWithoutExceptions();
+		Verify.that(bean::isTrue).valueIsEqualTo(true);
+	}
+
+	@Test
+	void testCreate_interface_carriesTypeAnnotationsFromParent()
+	{
+		InterfaceBean bean = Beans.create(InterfaceBean.class);
+
+		Verify.that(bean.getClass().isAnnotationPresent(TypeAnnotation.class)).isTrue();
+	}
+
+	@Test
+	void testCreate_interface_carriesMethodAnnotationsFromParent()
+	{
+		InterfaceBean bean = Beans.create(InterfaceBean.class);
+
+		Method method = Try.to(() -> bean.getClass().getDeclaredMethod("getString"));
+
+		Verify.that(method.isAnnotationPresent(MethodAnnotation.class)).isTrue();
+	}
+
+	@Test
+	void testCreate_reusesDynamicClass()
+	{
+		Verify.that(Beans.create(InterfaceBean.class)::getClass).suppliesNonUniqueValues();
 	}
 
 	public static class NotABean
@@ -68,9 +91,10 @@ public class BeansTest extends UtilTestBase {
 
 	}
 
-	@FooAnnotation
+	@TypeAnnotation
 	public interface InterfaceBean
 	{
+		@MethodAnnotation
 		String getString();
 
 		void setString(String string);
@@ -78,17 +102,18 @@ public class BeansTest extends UtilTestBase {
 		boolean isTrue();
 
 		void setTrue(boolean bool);
-
-		Object notAGetter();
-
-		void getKiddingItsVoid();
-
-		Object getWaitIsThisASetter(Object object);
 	}
 
 	@Retention(RetentionPolicy.RUNTIME)
 	@Target(ElementType.TYPE)
-	public @interface FooAnnotation
+	public @interface TypeAnnotation
+	{
+
+	}
+
+	@Retention(RetentionPolicy.RUNTIME)
+	@Target(ElementType.METHOD)
+	public @interface MethodAnnotation
 	{
 
 	}
