257ef3d23228822181cd30ac1bf70ea83abdfedf
Adam Harrison <ad@mharrison.us>
2017-07-21 18:33:24 -0400

    Correct imports

--- src/test/java/com/ulfric/dragoon/application/FeatureTest.java
+++ src/test/java/com/ulfric/dragoon/application/FeatureTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
+import org.mockito.Matchers;
 import org.mockito.Mockito;
 
 import com.google.common.truth.Truth;
@@ -33,14 +34,14 @@ class FeatureTest {
 
 	@Test
 	void testDoNothingWrapper() {
-		Mockito.when(feature.apply(Mockito.any())).thenReturn(null);
+		Mockito.when(feature.apply(Matchers.any())).thenReturn(null);
 		Truth.assertThat(Feature.wrap(new Object())).isNull();
 	}
 
 	@Test
 	void testNewApplicationWrapper() {
 		Application expected = new Application();
-		Mockito.when(feature.apply(Mockito.any())).thenReturn(expected);
+		Mockito.when(feature.apply(Matchers.any())).thenReturn(expected);
 		Truth.assertThat(Feature.wrap(new Object())).isSameAs(expected);
 	}
 
