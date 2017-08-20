9bf225187c095b79ec71d809863badedf30826c8
Adam Harrison <ad@mharrison.us>
2017-08-19 20:55:55 -0400

    Lazy retrying

--- src/main/java/com/ulfric/dragoon/value/Lazy.java
+++ src/main/java/com/ulfric/dragoon/value/Lazy.java
@@ -8,26 +8,40 @@ public final class Lazy<T> implements Supplier<T> {
 	public static <T> Lazy<T> of(Supplier<T> supplier) {
 		Objects.requireNonNull(supplier, "supplier");
 
-		return new Lazy<>(supplier);
+		return new Lazy<>(supplier, false);
+	}
+
+	public static <T> Lazy<T> ofRetrying(Supplier<T> supplier) {
+		Objects.requireNonNull(supplier, "supplier");
+
+		return new Lazy<>(supplier, true);
 	}
 
 	private final Supplier<T> supplier;
+	private final boolean retry;
 	private T value;
 	private boolean called;
 
-	private Lazy(Supplier<T> supplier) {
+	private Lazy(Supplier<T> supplier, boolean retry) {
 		this.supplier = supplier;
+		this.retry = retry;
 	}
 
 	@Override
 	public T get() {
-		if (this.called) {
-			return this.value;
+		if (called) {
+			if (shouldRetry()) {
+				return value;
+			}
 		}
 
-		this.called = true;
-		this.value = this.supplier.get();
-		return this.value;
+		called = true;
+		value = supplier.get();
+		return value;
+	}
+
+	private boolean shouldRetry() {
+		return this.retry && this.value == null;
 	}
 
 }
